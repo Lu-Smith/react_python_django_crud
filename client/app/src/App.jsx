218a2687a22c48e6c1dfc5caa96b0314489a1f3f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-12 23:54:53 +0100

    Added basic content to App.jsx

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -4,6 +4,11 @@ const App = ()  => {
   return (
     <>
       <h1>Books</h1>
+      <form>
+        <input type="text" placeholder='Book title...' />
+        <input type="number" placeholder='Year...' />
+        <button>Add book</button>
+      </form>
     </>
   )
 }
