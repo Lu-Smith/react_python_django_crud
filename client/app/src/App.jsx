b2c3b789b93b2c412193183fd564d1b38b52e938
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-13 18:47:25 +0100

    Updated App.jsx
    Added functionality to the form

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 
 const App = ()  => {
   const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [year, setYear] = useState(1950);
 
   useEffect(() => {
     fetchBooks();
@@ -16,16 +18,47 @@ const App = ()  => {
     }
     catch(error) {
       console.log(error);
+    } 
+  };
+
+  const addBook = async () => {
+    const bookData = {
+      title,
+      year
+    };
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/create/", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bookData),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch(error) {
+      console.log(error);
     }
-  }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    addBook(); 
+  };
 
   return (
     <div className='App'>
       <h1>Books</h1>
-      <form>
-        <input type="text" placeholder='Book title...' />
-        <input type="number" placeholder='Year...' />
-        <button>Add book</button>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder='Book title...' 
+        onChange={(e) => setTitle(e.target.value)} />
+        <input 
+        type="number" 
+        placeholder='Year...' 
+        onChange={(e) => setYear(Number(e.target.value))} />
+        <button type="submit">Add book</button>
       </form>
       <div className='booksContainer'>
         {books.map((book) => (
