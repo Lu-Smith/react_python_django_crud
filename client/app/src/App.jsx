763d11ac3f9d4a7a6c7a9287e8ae562a612eafa5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-13 17:07:36 +0100

    Installed cors

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,6 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 
 const App = ()  => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, [])
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
+      const data = await response.json()
+      console.log(data);
+    }
+    catch(error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <h1>Books</h1>
