0e671197baaa0f314894b869fe607d841669922a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-13 17:38:35 +0100

    Updated App.jsx

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import './App.css';
 
 const App = ()  => {
   const [books, setBooks] = useState([]);
@@ -10,8 +11,8 @@ const App = ()  => {
   const fetchBooks = async () => {
     try {
       const response = await fetch("http://127.0.0.1:8000/api/books/");
-      const data = await response.json()
-      console.log(data);
+      const data = await response.json();
+      setBooks(data);
     }
     catch(error) {
       console.log(error);
@@ -19,14 +20,22 @@ const App = ()  => {
   }
 
   return (
-    <>
+    <div className='App'>
       <h1>Books</h1>
       <form>
         <input type="text" placeholder='Book title...' />
         <input type="number" placeholder='Year...' />
         <button>Add book</button>
       </form>
-    </>
+      <div className='booksContainer'>
+        {books.map((book) => (
+          <div key={book.id} className='book'>
+            <p>Title: {book.title}</p>
+            <p>Year: {book.year}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
