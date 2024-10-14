c6dbc480455554a19b07f9b49a1d8509fb0990f1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-14 10:40:40 +0100

    Created BookContext.jsx

---
+++ client/app/src/components/BookContext.jsx
@@ -0,0 +1,55 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const BookContext = createContext();
+
+export const useBooks = () => useContext(BookContext);
+
+export const BookProvider = ({ children }) => {
+  const [books, setBooks] = useState([]); 
+  const [newTitle, setNewTitle] = useState("");
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
+      const data = await response.json();
+      setBooks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateTitle = async (pk, year) => {
+    const bookData = { title: newTitle, year };
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "PUT",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(bookData),
+      });
+      const data = await response.json();
+      setBooks((prev) =>
+        prev.map((book) => (book.id === pk ? data : book))
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <BookContext.Provider
+      value={{
+        newTitle,
+        setNewTitle,
+        updateTitle,
+        books,       
+        setBooks
+      }}
+    >
+      {children}
+    </BookContext.Provider>
+  );
+};
